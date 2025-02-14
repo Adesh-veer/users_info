f38825dd172fbd5784cfc0815181844365bbd5e4
Adesh-veer <adeshveer99@gmail.com>
2025-02-14 15:31:08 +0530

    Updated features

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -1,4 +1,4 @@
-import User from '../models/user.models.js'; // Assuming your User model is also using ES modules or is compatible
+import User from '../models/user.models.js'; 
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
@@ -6,15 +6,15 @@ export const register = async (req, res) => {
   try {
     const { username, password, fullName, gender, DOB, country } = req.body;
 
-    // Check if user already exists
+
     if (await User.findOne({ username })) {  
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    // Hash password
+   
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create user
+   
     const newUser = await User.create({
       username, 
       password: hashedPassword, 
@@ -34,15 +34,14 @@ export const login = async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    // Check user
     const user = await User.findOne({ username });
     if (!user) return res.status(400).json({ message: 'Invalid credentials' });
 
-    // Validate password
+   
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-    // Generate JWT token
+    
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
     res.json({ message: 'Login successful', token });
